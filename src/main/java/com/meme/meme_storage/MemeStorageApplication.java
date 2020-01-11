6f06afb23a4f836d0aa2f3b1e801f80d3bb9d3b0
HJungIn <gjwjddls96@naver.com>
2020-01-11 17:46:10 +0900

    Initial commit

---
+++ src/main/java/com/meme/meme_storage/MemeStorageApplication.java
@@ -0,0 +1,13 @@
+package com.meme.meme_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meme/meme_storage/MemeStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meme.meme_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
