03ef780914035dfbdac9fcaaac1f1e38cac43d21
HJungIn <gjwjddls96@naver.com>
2020-01-18 13:35:28 +0900

    이미지 경로위치 재설정, method 이름 재설정, 검색 태그에 해당하는 이미지가 없을 때의 오류 해결

--- src/main/java/com/meme/meme_storage/controller/FileController.java
+++ src/main/java/com/meme/meme_storage/controller/FileController.java
@@ -7,7 +7,6 @@ import com.meme.meme_storage.domain.file.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Controller
@@ -34,52 +30,44 @@ public class FileController {
     private final FileService fileService;
 
     @RequestMapping("/file")
-    public String fileSelect(){
+    public String fileSelect() {
         return "file";
     }
 
 
     @PostMapping("/upload")
-    public String upload( HttpServletRequest request,
-                         @RequestParam("file") MultipartFile multipartFile,
-                          @RequestParam("tagName") List<String> tagNames ) throws IOException{
+    public String upload(@RequestParam("file") MultipartFile multipartFile,
+                         @RequestParam("tagName") List<String> tagNames) throws IOException {
 
 
-        Path fileLocation = fileService.pathSetting();
-        Path targetLocation = fileLocation.resolve(multipartFile.getOriginalFilename());
-        Files.copy(multipartFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        Path targetLocation = fileService.uploadPathSetting(multipartFile);
 
         MemeFile memeFile = new MemeFile(multipartFile.getOriginalFilename(), multipartFile.getContentType(), targetLocation.toString(), multipartFile.getSize());
         fileService.saveMemeFile(memeFile);
 
 
-
         for (String tagName : tagNames) {
-            Tag tag1 = new Tag(tagName);
-            fileService.saveTag(tag1);
+            Tag tag = new Tag(tagName);
+            fileService.saveTag(tag);
 
-            fileService.saveMemeFileTag( MemeFileTag.createMemeFileTag(memeFile,tag1) );
+            fileService.saveMemeFileTag(MemeFileTag.createMemeFileTag(memeFile, tag));
         }
-
-
 
         return "redirect:/";
     }
 
     @GetMapping("/files")
-    public String fileList(Model model){
-        List<MemeFile> files= fileService.findAllFiles();
+    public String fileList(Model model) {
+        List<MemeFile> files = fileService.findAllFiles();
         model.addAttribute("files", files);
         return "files";
     }
 
     @PostMapping("/download")
-    public ResponseEntity<Resource> download( @RequestParam("fileId") Long id) throws IOException{
+    public ResponseEntity<Resource> download(@RequestParam("fileId") Long id) throws IOException {
 
-        MemeFile file = fileService.findById(id);
-        Path fileLocation = fileService.pathSetting();
-        Path filePath = fileLocation.resolve(file.getFileName()).normalize();
-        Resource resource = new UrlResource(filePath.toUri());
+        MemeFile file = fileService.getMemeFileById(id);
+        Resource resource = fileService.downPathSetting(file);
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(file.getFileType()))
@@ -92,9 +80,9 @@ public class FileController {
      */
 
     @GetMapping("/search")
-    public String tagSearch(@RequestParam("tagName") String tagName, Model model){
+    public String tagSearch(@RequestParam("tagName") String tagName, Model model) {
 
-        List<MemeFile> files= fileService.findMemeFileByTag(tagName);
+        List<MemeFile> files = fileService.findMemeFileByTag(tagName);
         model.addAttribute("files", files);
 
         return "files";
--- src/main/java/com/meme/meme_storage/domain/file/service/FileService.java
+++ src/main/java/com/meme/meme_storage/domain/file/service/FileService.java
@@ -2,15 +2,22 @@ package com.meme.meme_storage.domain.file.service;
 
 import com.meme.meme_storage.domain.file.entity.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,15 +36,28 @@ public class FileService {
         return memeFileRepository.findAll();
     }
 
-    public MemeFile findById(Long id) {
+    public MemeFile getMemeFileById(Long id) {
+        Optional<MemeFile> memeFiles = memeFileRepository.findById(id);
+        if (!memeFiles.isPresent()) {
+            return new MemeFile("", "", "", 0);
+        }
+
         return memeFileRepository.findById(id).get();
     }
 
 
-    public Path pathSetting() throws IOException {
-        Path path = Paths.get("resources/static/images").normalize();
-        Files.createDirectories(path);
-        return path;
+    public Path uploadPathSetting(MultipartFile multipartFile) throws IOException {
+        Path fileLocation = Paths.get("src/main/upload/static/images").normalize();
+        Path targetLocation = fileLocation.resolve(multipartFile.getOriginalFilename());
+        Files.copy(multipartFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        return targetLocation;
+    }
+
+    public Resource downPathSetting(MemeFile file) throws MalformedURLException {
+        Path fileLocation = Paths.get("src/main/upload/static/images").normalize();
+        Path filePath = fileLocation.resolve(file.getFileName()).normalize();
+        Resource Resource = new UrlResource(filePath.toUri());
+        return Resource;
     }
 
     public void saveTag(Tag tag) {
@@ -48,10 +68,19 @@ public class FileService {
         memeFileTagRepository.save(memeFileTag);
     }
 
-    public List<MemeFile> findMemeFileByTag(String tagName){
-
+    public List<MemeFile> findMemeFileByTag(String tagName) {
         List<Tag> tags = tagRepository.findByTagName(tagName);
 
-        return memeFileTagRepository.findByTag(tags);
+        if (tags.size() == 0) {
+            List<MemeFile> emptyFiles = new ArrayList<>();
+            return emptyFiles;
+        }
+
+        List<MemeFile> memeFiles = memeFileTagRepository.findByTag(tags);
+
+
+        return memeFiles;
     }
+
+
 }
